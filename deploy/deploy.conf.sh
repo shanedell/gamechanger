8387f74ca2fa368b20d10064ec554e8b9c0b0ad3
Shane Dell <shanedell100@gmail.com>
2024-07-11 21:26:47 +0000

    Remove running of non-existent sql file. Add volume for needed shell script

--- deploy/deploy.conf.sh
+++ deploy/deploy.conf.sh
@@ -18,17 +18,17 @@ declare -a REPO_NAMES=(\
 )
 
 declare -A REPO_URL_MAP=(\
-  [gamechanger-web]="https://github.com/dod-advana/gamechanger-web" \
-  [gamechanger-data]="https://github.com/dod-advana/gamechanger-data.git" \
-  [gamechanger-ml]="https://github.com/dod-advana/gamechanger-ml.git" \
+  [gamechanger-web]="https://github.com/shanedell/gamechanger-web" \
+  [gamechanger-data]="https://github.com/shanedell/gamechanger-data.git" \
+  [gamechanger-ml]="https://github.com/shanedell/gamechanger-ml.git" \
   [gamechanger-neo4j-plugin]="https://github.com/dod-advana/gamechanger-neo4j-plugin.git" \
   [gamechanger-crawlers]="https://github.com/dod-advana/gamechanger-crawlers.git" \
 )
 
 declare -A REPO_TAG_MAP=(\
-  [gamechanger-web]="dev" \
-  [gamechanger-data]="dev" \
-  [gamechanger-ml]="dev" \
+  [gamechanger-web]="patch-yarnlock-docker-prod" \
+  [gamechanger-data]="patch-dockerfiles" \
+  [gamechanger-ml]="patch-reqs" \
   [gamechanger-neo4j-plugin]="main" \
   [gamechanger-crawlers]="dev" \
 )
